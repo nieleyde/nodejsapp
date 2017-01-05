be4c4f5f6f8271b75bbe01a08f59fe36737bcf36
Niel Eyde <nieleyde@gmail.com>
2017-01-05 18:27:07 +0000

    openshift mod

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var injection = require('allcountjs');
-var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-var cfenv = require("cfenv");
+//var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+//var cfenv = require("cfenv");
 console.log('START------->');
-console.log(cfenv.getAppEnv().getService('mymongo'));
+//console.log(cfenv.getAppEnv().getService('mymongo'));
+console.log(process.env.mongouri);
 console.log('END------->');
 injection.bindFactory('port', process.env.PORT || 3000);
-//injection.bindFactory('dbUrl', 'mongodb://.......');
+injection.bindFactory('dbUrl', process.env.mongouri);
 //injection.bindFactory('dbUrl', vcap_services.mlab[0].credentials.uri);
-injection.bindFactory('dbUrl', cfenv.getAppEnv().getService('mymongo').credentials.uri);
+//injection.bindFactory('dbUrl', cfenv.getAppEnv().getService('mymongo').credentials.uri);
 injection.bindFactory('gitRepoUrl', './app-config');
 var server = injection.inject('allcountServerStartup');
 server.startup(function (errors) {
